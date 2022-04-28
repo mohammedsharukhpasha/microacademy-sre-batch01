e3c0f3a52fa977200bac89e1bdf0e08d8b0f4d83
mohammedsharukhpasha <104357663+mohammedsharukhpasha@users.noreply.github.com>
2022-04-28 11:50:08 +0530

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,20 @@
+r the changes (see ":help recovery").
+#!/bin/bash
+
+echo"This is a simple math program"
+
+echo "we are going to perform addition"
+
+echo "Enter first value for addition,e.g.x=a"
+
+read x
+
+echo " Enter second value for addition ,e.g.y=b"
+
+read y
+
+sum =$($x+$y)
+
+echo "The result for addition"
+
+echo $sum
