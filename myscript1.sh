ef754fc20a92f6d6737e6d2ad997b0f54e46d439
mohammedsharukhpasha <104357663+mohammedsharukhpasha@users.noreply.github.com>
2022-04-28 12:57:14 +0530

    myscript.sh

---
+++ myscript1.sh
@@ -0,0 +1,25 @@
+i#!/bin/bash
+
+echo"This is a simple math program"
+
+echo "we are going to perform addition,subtraction,multiplication,division"
+
+echo "Enter first value for addition,subtraction,multiplication,division,e.g.x=a"
+
+read x
+
+echo " Enter second value for addition,subtraction,multiplication,division,e.g.y=b"
+
+
+read y
+
+sum 1=$($x+$y)
+
+sum 2 =$($x-$y)
+
+sum 3=$($x*$Y)
+
+echo "The result for addition,subtraction,multiplication, division"
+
+echo $sum1,$sum2,$sum3
+~                                           
