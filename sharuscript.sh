3d07e8bdfe17dca28ed916d291f31030852888c0
mohammedsharukhpasha <104357663+mohammedsharukhpasha@users.noreply.github.com>
2022-04-29 10:04:27 +0530

    sharuscript.sh

---
+++ sharuscript.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "this is a sample print script"
+
+$printf %s \n"hello world"
+
+$crontabe-e
+
+1****/pscript.sh
+~                                                                                                                                                             
+~          
